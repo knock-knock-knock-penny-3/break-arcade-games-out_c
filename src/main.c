a22fa13a299978d5b2370e4daee1bf8e5ce8cf38
Enrico Sanna <antani@tiscali.it>
2023-11-08 11:29:26 +0100

    creating game window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include "main.h"
 
 int main() {
+    SDL_Window *window;
 
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow(
+                                WINDOW_TITLE,               // window title
+                                SDL_WINDOWPOS_UNDEFINED,    // initial x position
+                                SDL_WINDOWPOS_UNDEFINED,    // initial y position
+                                SCREEN_WIDTH,               // width, in pixels
+                                SCREEN_HEIGHT,              // height, in pixels
+                                SDL_WINDOW_RESIZABLE        // flags
+                            );
+
+//    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 
     return 0;
 }
--- src/main.h
+++ src/main.h
@@ -1,6 +1,20 @@
 #ifndef BREAKOUT_MAIN_H_INCLUDED
 #define BREAKOUT_MAIN_H_INCLUDED
 
+#include "SDL2/SDL.h"
 
+typedef int b32;
+
+#define true 1
+#define false 0
+
+#define global_variable static
+#define internal static
+
+static int running = true;
+
+#define WINDOW_TITLE    "Breakout"
+#define SCREEN_WIDTH    1280
+#define SCREEN_HEIGHT   720
 
 #endif // BREAKOUT_MAIN_H_INCLUDED
